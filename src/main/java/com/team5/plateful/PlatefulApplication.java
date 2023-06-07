1af6cd2bc979d1ecbcbe64ff504c74b11e49fcd4
John Buhr <Johnbuhr92@gmail.com>
2023-06-07 12:49:56 -0500

    fixed deployment of springboot and navbar partial

---
+++ src/main/java/com/team5/plateful/PlatefulApplication.java
@@ -0,0 +1,13 @@
+package com.team5.plateful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatefulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatefulApplication.class, args);
+    }
+
+}
