7439ee035c9fdaa53537fec8ed6f7a6af7516afc
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-06-08 09:39:09 -0500

    adding register functionality for users

--- src/main/java/com/team5/plateful/controllers/UserController.java
+++ src/main/java/com/team5/plateful/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.team5.plateful.controllers;
 
+import com.team5.plateful.models.User;
 import com.team5.plateful.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -16,5 +19,11 @@ public class UserController {
         return "/register";
     }
 
+    @PostMapping("/register")
+    public String registerUser(@RequestParam(name="username") String username, @RequestParam(name="email") String email, @RequestParam(name="password") String password, @RequestParam(name="avatar_url") String avatar_url) {
+        User user = new User(username, email, password, avatar_url);
+        usersDao.save(user);
+        return "redirect:/login";
+    }
 }
 
