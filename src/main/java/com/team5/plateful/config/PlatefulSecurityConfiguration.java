9a6af6b7bdfc57fb73eb293a0fa334f816453ab6
John Buhr <Johnbuhr92@gmail.com>
2023-06-13 13:42:25 -0500

    created loading image and fixed recipe page footer

--- src/main/java/com/team5/plateful/config/PlatefulSecurityConfiguration.java
+++ src/main/java/com/team5/plateful/config/PlatefulSecurityConfiguration.java
@@ -16,8 +16,27 @@ public class PlatefulSecurityConfiguration {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
 
-                .requestMatchers("/recipes/create", "/recipes/*/view", "/recipes/comment/", "/recipes/*/edit", "/profile/update", "/profile", "/recipes/search/create").authenticated()
-                .requestMatchers("/recipes", "/recipes/{id}", "/register", "/login", "/about", "/static", "/recipes/search", "/logout", "/").permitAll()
+                .requestMatchers(
+                        "/recipes/create",
+                        "/recipes/*/view",
+                        "/recipes/comment/",
+                        "/recipes/*/edit",
+                        "/profile/update",
+                        "/profile",
+                        "/recipes/search/create"
+                ).authenticated()
+                .requestMatchers(
+                        "/recipes",
+                        "/recipes/{id}",
+                        "/register",
+                        "/login",
+                        "/about",
+                        "/static",
+                        "/recipes/search",
+                        "/logout",
+                        "/loading",
+                        "/"
+                ).permitAll()
                 .requestMatchers("/css/**", "/js/**").permitAll()
         );
         http.formLogin((form) -> form
