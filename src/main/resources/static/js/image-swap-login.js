62ae4e5c13725870f3cf46d484c7f6d15c7a11c0
Marc Mata <marcmata26@gmail.com>
2023-07-05 15:39:52 -0500

    changed variable to cover more aspects

--- src/main/resources/static/js/image-swap-login.js
+++ src/main/resources/static/js/image-swap-login.js
@@ -8,7 +8,9 @@ window.addEventListener("resize", function() {
     }
 });
 
-if (window.innerWidth <= 768) {
+let screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+if (screenWidth <= 768) {
     image.src = "/css/img/gathering-2.jpg"
 } else {
     image.src = "/css/img/wings.jpg";
