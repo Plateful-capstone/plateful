67926dac7c12f08111c983209868465f8b917969
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-07-06 09:21:45 -0500

    css changes

--- src/main/resources/static/js/image-swap-login.js
+++ src/main/resources/static/js/image-swap-login.js
@@ -7,3 +7,15 @@ window.addEventListener("resize", function() {
         image.src = "/css/img/wings.jpg";
     }
 });
+
+// / Check the screen resolution
+// var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+// var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+// // Change a property based on the screen resolution
+// if (screenWidth < 1024 && screenHeight < 768) {
+//     // Set property for smaller screens
+//     document.body.style.backgroundColor = 'red';
+// } else {
+//     // Set property for larger screens
+//     document.body.style.backgroundColor = 'blue';
+// }
